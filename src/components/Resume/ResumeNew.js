217fc31b15665b1651f6bb3975f9488d6de40cce
HarryLee02 <22520435@gm.uit.edu.vn>
2025-08-20 09:53:28 +0700

    Update resumé

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV_2025_LeNgocHieu_eng.pdf";
+import pdf from "../../Assets/../Assets/CV_8-2025_LeNgocHieu_eng.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
